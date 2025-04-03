3318ab7babd627bfc7c538f838e188767e78d1cf
dbYTE <kuznetsovromanp@gmail.com>
2025-04-03 22:25:39 +0300

    KS-1-5 alternative solution of task5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -6,13 +6,11 @@ const val minutesInHour: Int = 60
 fun main() {
     val secondsInSpace: Int = 6480
 
-    val hoursInSpace: Int
-    val minutesInSpace: Int
+    val hoursInSpace: Int = secondsInSpace / (secondsInMinute * minutesInHour)
+    val minutesInSpace: Int = secondsInSpace / secondsInMinute
 
-    minutesInSpace = secondsInSpace / secondsInMinute
-    hoursInSpace = minutesInSpace / minutesInHour
-    val minutesInSpaceLeft: Int = minutesInSpace - (hoursInSpace * minutesInHour)
-    val secondsInSpaceLeft: Int = secondsInSpace - ((minutesInSpaceLeft * secondsInMinute) + (hoursInSpace * (minutesInHour * secondsInMinute)))
+    val minutesInSpaceLeft: Int = minutesInSpace % (hoursInSpace * minutesInHour)
+    val secondsInSpaceLeft: Int = secondsInSpace % (minutesInSpace * secondsInMinute)
 
     println(String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpaceLeft, secondsInSpaceLeft))
 }
