9a276b097db2c64f04369bf0f8c9600b6eca64b1
dbYTE <kuznetsovromanp@gmail.com>
2025-04-03 20:05:53 +0300

    KS-1-5 fix for solution of task5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,18 @@
 package org.example.lesson_1
 
+const val secondsInMinute: Int = 60
+const val minutesInHour: Int = 60
+
 fun main() {
-    val secondsInSpace: Int = 6480 // сколько СЕКУНД Гагарин пробыл в Космосе (из задачи №4)
+    val secondsInSpace: Int = 6480
 
-    val hoursInSpace: Int // сколько ЧАСОВ Гагарин пробыл в Космосе
-    val minutesInSpace: Int // сколько МИНУТ Гагарин пробыл в Космосе
+    val hoursInSpace: Int
+    val minutesInSpace: Int
 
-    minutesInSpace = secondsInSpace / 60 // вычисляем минуты в космосе деля количество секунд на 60 (кол-во секунд/мин)
-    hoursInSpace = minutesInSpace / 60 // вычисляем часы в космосе деля количество минут на 60 (кол-во минут/час)
-    val minutesInSpaceLeft: Int =
-        minutesInSpace - (hoursInSpace * 60) // объявляем переменную и записываем в неё остаток минут за вычетом часа
-    val secondsInSpaceLeft: Int =
-        secondsInSpace - ((minutesInSpaceLeft * 60) + (hoursInSpace * (60 * 60))) // объявляем переменную и записываем в неё остаток секунд за вычетом часов и минут
+    minutesInSpace = secondsInSpace / secondsInMinute
+    hoursInSpace = minutesInSpace / minutesInHour
+    val minutesInSpaceLeft: Int = minutesInSpace - (hoursInSpace * minutesInHour)
+    val secondsInSpaceLeft: Int = secondsInSpace - ((minutesInSpaceLeft * secondsInMinute) + (hoursInSpace * (minutesInHour * secondsInMinute)))
 
-    println("0$hoursInSpace:$minutesInSpaceLeft:0$secondsInSpaceLeft")
+    println(String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpaceLeft, secondsInSpaceLeft))
 }
