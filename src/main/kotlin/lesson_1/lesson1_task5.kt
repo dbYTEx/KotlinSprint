ffa8f227b71a7beb30fa438de665cacc1f10835c
dbYTE <kuznetsovromanp@gmail.com>
2025-04-03 23:37:26 +0300

    KS-1-5 uppercase for const fix

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_1
 
-const val secondsInMinute: Int = 60
-const val minutesInHour: Int = 60
+const val SECONDS_IN_MINUTE: Int = 60
+const val MINUTES_IN_HOUR: Int = 60
 
 fun main() {
     val secondsInSpace: Int = 6480
 
-    val hoursInSpace: Int = secondsInSpace / (secondsInMinute * minutesInHour)
-    val minutesInSpace: Int = secondsInSpace / secondsInMinute
+    val hoursInSpace: Int = secondsInSpace / (SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
+    val minutesInSpace: Int = secondsInSpace / SECONDS_IN_MINUTE
 
-    val minutesInSpaceLeft: Int = minutesInSpace % (hoursInSpace * minutesInHour)
-    val secondsInSpaceLeft: Int = secondsInSpace % (minutesInSpace * secondsInMinute)
+    val minutesInSpaceLeft: Int = minutesInSpace % (hoursInSpace * MINUTES_IN_HOUR)
+    val secondsInSpaceLeft: Int = secondsInSpace % (minutesInSpace * SECONDS_IN_MINUTE)
 
     println(String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpaceLeft, secondsInSpaceLeft))
 }
