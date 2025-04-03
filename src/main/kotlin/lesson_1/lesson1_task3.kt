df1cecd57e7a7af1d45c1e20a9e75879cad2c9c7
dbYTE <kuznetsovromanp@gmail.com>
2025-04-03 19:34:55 +0300

    KS-1-3 fix for solution of task3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,15 +2,15 @@ package org.example.lesson_1
 
 fun main() {
     val yearOfFlight: Int = 1963
-    var hourOfFlight: Int = 9
-    var minuteOfFlight: Int = 7
+    var hourOfFlight: String = "09"
+    var minuteOfFlight: String = "07"
 
     println(yearOfFlight)
     println(hourOfFlight)
     println(minuteOfFlight)
 
-    hourOfFlight = 10
-    minuteOfFlight = 55
+    hourOfFlight = "10"
+    minuteOfFlight = "55"
 
     print(hourOfFlight)
     print(":")
