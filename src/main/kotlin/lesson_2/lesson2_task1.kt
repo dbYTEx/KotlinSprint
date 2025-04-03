5f008d4c3fbd3520fbe6e95fbaeef92223b3297b
dbYTE <kuznetsovromanp@gmail.com>
2025-04-03 23:03:59 +0300

    KS-2-1 solution of task1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val firstStudent: Int = 3
+    val secondStudent: Int = 4
+    val thirdStudent: Int = 3
+    val fourthStudent: Int = 5
+
+    val avgGrade: Double = (firstStudent + secondStudent + thirdStudent + fourthStudent) / 4.0
+
+    println(avgGrade)
+}
