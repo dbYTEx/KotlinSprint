b5c635184d8cb3a7265c60f65ab8b972d2e1f65c
dbYTE <kuznetsovromanp@gmail.com>
2025-04-07 11:51:41 +0300

    KS-4-1 fix for solution of task 1

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,9 +1,11 @@
 package org.example.lesson_4
 
+const val NUMBER_OF_TABLES: Int = 13
+
 fun main() {
-    val totalNumberOfTables: Int = 13
-    val reservedTablesToday: Int = totalNumberOfTables
+    val reservedTablesToday: Int = NUMBER_OF_TABLES
     val reservedTablesTomorrow: Int = 9
 
-    println("Доступность столиков на сегодня: ${totalNumberOfTables > reservedTablesToday}\nДоступность столиков на завтра: ${totalNumberOfTables > reservedTablesTomorrow}")
+    println("Доступность столиков на сегодня: ${NUMBER_OF_TABLES > reservedTablesToday}\n" +
+            "Доступность столиков на завтра: ${NUMBER_OF_TABLES > reservedTablesTomorrow}")
 }
